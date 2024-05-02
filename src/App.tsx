fc37a9ad5f9052f0d40cf67ace4cbe54b2b76a45
Vladislava <stdemidovez@gmail.com>
2024-05-02 20:08:29 +0300

    layout Login + Registration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import {Login} from "./components/Login/Login.tsx";
+import {Registration} from "./components/Registration/Registration.tsx";
+
+function App() {
+  return (
+      <>
+        {/*<Login/>*/}
+          <Registration />
+      </>
+  )
+
+}
+
+export default App
