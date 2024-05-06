d4ef93493dfb5bea15f270bb2de223fad8c14f6b
Vladislava <stdemidovez@gmail.com>
2024-05-06 12:52:12 +0300

    connect redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
 import {GlobalStyled} from "./styles/Global.styled.ts";
+import {Provider} from "react-redux";
+import store from "./redux/store.ts";
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <GlobalStyled/>
         <BrowserRouter>
-            <App/>
+            <Provider store={store}>
+                <App/>
+            </Provider>
         </BrowserRouter>
     </React.StrictMode>,
 )
